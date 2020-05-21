f63949d22523ea225cbe7aa7455e694bc3c71ed6
TomKugelman <52869328+TomKugelman@users.noreply.github.com>
2020-05-21 14:43:20 -0400

    Added Searching Implamentation
    Potentially the final commit
    
    TODO: At the index of the searched number once found to BinarySearch()  return function

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Author: Thomas Kugelman
 // Language: C#
+// Framework: .NET 4.7.2
+//-------------------------------------------------------------------------------
 // Description: A few simple sorting algorithms that demonstrate how important efficiency is.
 // I originally wrote a program like this for a class and it was a very eye opening experience for me.
 // This is a revised version of that program.
@@ -15,22 +17,20 @@ namespace Sorting_Algorithms
         static void Main(string[] args)
         {
             #region Constants
-            int MAX = 10;
-            int range = 100;
+            int MAX = 6000;
+            int range = 10000;
+            Stopwatch stopWatch = new Stopwatch();
             #endregion
 
-            Stopwatch stopWatch = new Stopwatch();
-
+            #region Data Members
             // Call function to fill array of <MAX> size within the <range> of numbers
             // NOTE: This fills with unique numbers only!
             // <range> MUST be HIGHER than <MAX>
             int[] array = FillArray(MAX, range);
 
-
-            #region Data Members
             /* My original program had each sorting function run one at a time via a while loop, cycling to the next
             sorting function after every loop iteration. I chose to instead create a seperate array for each sorting function that copies the original
-            <array>. While this takes up more total memory, for the purpose of this project I prefer using a more obvious method.*/
+            <array>. While this takes up more total memory, for the purpose of this project I prefer using a more obvious separation of each function.*/
             int[] insertionSortArray = new int[MAX];
             int[] bubbleSortArray = new int[MAX];
             int[] quickSortArray = new int[MAX];
@@ -47,16 +47,22 @@ namespace Sorting_Algorithms
             string linearTime = "";
             string binaryTime = "";
 
+            string linearResult = "";
+            string binaryResult = "";
+
             string[] sortTimerArray = { insertionTime, bubbleTime, quickTime, selectionTime, mergeTime };
             string[] searchTimerArray = { linearTime, binaryTime };
 
             string[] sortingFunctionNames = { "Insertion Sort", "Bubble Sort", "Quick Sort", "Selection Sort", "Merge Sort" };
+            string[] searchFunctionNames = { "Linear Search", "Binary Search" };
 
             // An array of the sorted arrays, will make outputting easier.
             int[][] sortedArrays = {
                 insertionSortArray, quickSortArray,
                 bubbleSortArray, selectionSortArray, mergeSortArray
             };
+
+            string[] searchResults = { linearResult, binaryResult };
 
             // Using a list of functions to process each array without duplicate code.
             List<Func<int[], int[]>> SortingFunctions = new List<Func<int[], int[]>>
@@ -97,7 +103,6 @@ namespace Sorting_Algorithms
 
             #endregion
 
-
             #region Output
             // Output original unsorted <array>
             Console.WriteLine("Original unsorted array\n");
@@ -118,8 +123,141 @@ namespace Sorting_Algorithms
                 }
                 Console.WriteLine("\n---------------------------------------------------------------------------------------\n");
             }
+
+            // Get input for searching functions
+            bool validInput = false;
+            int input = 0;
+
+            while (!validInput)
+            {
+                Console.WriteLine("\nWhat positive integer would you like to search for?\n");
+                try
+                {                    
+                    input = Convert.ToInt32(Console.ReadLine());
+
+                    validInput = true;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is FormatException || ex is OverflowException)
+                    {
+                        validInput = false;
+                        Console.WriteLine("Invalid Input! Please try again.");
+                    }
+                }
+            }
+
+            // Call searching functions
+            
+
+            stopWatch.Start();
+            searchResults[0] = LinearSearch(insertionSortArray, input);
+            stopWatch.Stop();
+            searchTimerArray[0] = stopWatch.Elapsed.ToString();
+            stopWatch.Reset();
+
+            stopWatch.Start();
+            searchResults[1] = BinarySearch(insertionSortArray, input);
+            stopWatch.Stop();
+            searchTimerArray[1] = stopWatch.Elapsed.ToString();
+            stopWatch.Reset();
+
+
+            // Output searching function results
+            for (int index = 0; index < searchFunctionNames.Length; index++)
+            {
+                Console.WriteLine("\n" + searchFunctionNames[index] + "\n" + searchTimerArray[index] + "\n\n"
+                    + searchResults[index]);
+                Console.WriteLine("\n---------------------------------------------------------------------------------------\n");
+            }
+
+            // Reiterate sorting function runtimes
+            for (int index = 0; index < sortingFunctionNames.Length; index++)
+            {
+                Console.WriteLine("\n" + sortingFunctionNames[index] + " runtime: " + sortTimerArray[index]);
+            }
+
             Console.ReadLine();
+            #endregion
         }
+
+        #region Searching Functions
+        private static string LinearSearch(int[] numArray, int input)
+        {
+            int targetIndex = 0;
+            bool foundInteger = false;
+            string result;
+
+            // Loops through array and at each index compares it tot eh user's <input>
+            for (int index = 0; index < numArray.Length; index++)
+            {
+                if (numArray[index] == input)
+                {
+                    targetIndex = index;
+                    foundInteger = true;
+                    break;
+                }
+            }
+
+            // Either return a string declaring the position of the searched integer or declare that it is not present
+            if (foundInteger)
+            {
+                result = "The integer you searched for is at index " + targetIndex.ToString() + ".";
+                return result;
+            }
+            else
+            {
+                result = "The integer you searched for is not in the array!";
+                return result;
+            }
+        }
+
+        private static string BinarySearch(int[] numArray, int input)
+        {
+            int middleIndex = numArray.Length / 2;
+            int[] childArray;
+            string result;
+
+            if (numArray[middleIndex] == input)
+            {
+                return "The integer you searched for is in the array!";
+            }
+            
+            if (numArray[0] == input)
+            {
+                result = "The integer you searched for is in the array!";
+                return result;
+            }
+            else if (numArray.Length == 1 && numArray[0] != input)
+            {
+                result = "The integer you searched for is not present.";
+                return result;
+            }
+
+            if (input < numArray[middleIndex])
+            {
+                childArray = new int[middleIndex];
+                for (int index = 0; index < childArray.Length; index++)
+                {
+                    childArray[index] = numArray[index];
+                }
+            }
+            else
+            {
+                childArray = new int[numArray.Length - middleIndex];
+                for (int index = 0; index < childArray.Length && middleIndex < numArray.Length; index++)
+                {
+                    childArray[index] = numArray[middleIndex];
+                    middleIndex++;
+                }
+            }
+
+            result = BinarySearch(childArray, input);
+            return result;
+        }
+
+
         #endregion
 
         #region Sorting Functions
@@ -168,20 +306,19 @@ namespace Sorting_Algorithms
 
             while (!allDone)
             {
+                // Check if either side has merged its full array, if both have finished break the loop.
                 if (indexLeft == leftArray.Length)
                     leftDone = true;
                 if (indexRight == rightArray.Length)
                     rightDone = true;
                 if (rightDone && leftDone)
                 {
-                    allDone = true;
+                    allDone = true; // This is redundant, but not important enough to change
                     break;
                 }
 
-                // First check if the opposite sides index has reached its array length, meaning it has no more integers to add
-                // Then if that isn't the case, check which side has the lowest integer at the head of the given array.
-
-
+                // Check which side's current head index has the lowest integer, 
+                // then add that side to final array and incrament the chosen side and the final array.
                 if (leftDone && !rightDone)
                 {
                     result[indexResult] = rightArray[indexRight];
